90037cd8a8ff92ac8f4fbde8fd4116ce2884ded1
rizalmuzaky108 <rizalmuzaky108@gmail.com>
2020-10-29 18:32:44 +0700

    1718108

---
+++ js/script.js
@@ -0,0 +1,210 @@
+const ApiKey = "c6f69e84fd144de79f4b19dc107e9556";
+const baseUrl = "https://api.football-data.org/v2/";
+const leagueId = "2021";
+const baseEndPoin = `${baseUrl}competitions/${leagueId}`;
+const teamEndPoin = `${baseUrl}competitions/${leagueId}/teams`;
+const standingEndPoin = `${baseUrl}competitions/${leagueId}/standings`;
+const matchEndPoin = `${baseUrl}competitions/${leagueId}/matches`;
+
+const contents = document.querySelector("#content-list");
+const title = document.querySelector(".card-title");
+const fetchHeader = {
+    headers: {
+        'X-Auth-Token': ApiKey
+    }
+};
+
+function getListTeams() {
+    title.innerHTML = "Daftar Tim Liga Primer Inggris";
+    fetch(teamEndPoin, fetchHeader)
+        .then(response => response.json())
+        .then(resJson => {
+            console.log(resJson.teams);
+            let teams = "";
+            resJson.teams.forEach(team => {
+                teams += `
+                <li class="collection-item avatar">
+                    <img src="${team.crestUrl}" alt="" class="circle">
+                    <span class="title">${team.name}</span>
+                    <p>Berdiri: ${team.founded} <br>
+                        Markas: ${team.venue}
+                    </p>
+                    <a href="#" id="getId${team.id}" data-id="${team.id}" data-target="modal1" class="secondary-content modal-trigger"><i class="material-icons">info</i></a>
+                </li>
+                `;
+            });
+            contents.innerHTML = '<ul class="collection">' + teams + '</ul>'
+            //kodingan tugas {
+            resJson.teams.forEach(team => {
+                document.getElementById(`getId${team.id}`).addEventListener('click', function () {
+                    showTeamInfo(this.dataset.id);
+                });
+            });
+            //}kodingan tugas
+        }).catch(err => {
+            console.error(err);
+        })
+}
+//kodingan tugas{
+function showTeamInfo(id) {
+    title.innerHTML = "";
+    let url = baseUrl + "teams/" + id;
+    fetch(url, fetchHeader)
+        .then(response => response.json())
+        .then(restJson => {
+            console.log(restJson)
+            let teams = "";
+            teams += `
+            <div class="container">
+            <h3 style="text-align: center;">${restJson.name}</h3>
+            <div class="card-image center-align">
+                    <img src="${restJson.crestUrl}" class="circle">
+                </div>
+            <div class="col s4">
+            <p style="font-weight: bold;">Nama Panjang</p>
+            <p style="font-weight: bold;">Asal</p>
+            <p style="font-weight: bold;">Jumlah Pemain</p>
+            <p style="font-weight: bold;">Website</p>
+            </div>
+            <div class="col s1">
+            <p>:</p>
+            <p>:</p>
+            <p>:</p>
+            <p>:</p>
+            </div>
+            <div class="col s7">
+                <p>${restJson.shortName}</p>
+                <p>${restJson.address}</p>
+                <p>${restJson.squad.length}</p>
+                <p>${restJson.website}</p>
+            </div>
+        </div>
+            `;
+            document.getElementById('content-list').innerHTML = `<div class="row">${teams}</div>`;
+        });
+}
+//}kodingan tugas
+
+function getListStandings() {
+    title.innerHTML = "Klasemen Sementara Liga Primer Inggris";
+    fetch(standingEndPoin, fetchHeader)
+        .then(response => response.json())
+        .then(resJson => {
+            console.log(resJson.standings[0]);
+            let teams = "";
+            let i = 1;
+            resJson.standings[0].table.forEach(team => {
+                teams += `
+                <tr>
+                    <td style="padding-left:20px;">${i}.</td>
+                    <td><img src="${team.team.crestUrl}" alt="${team.team.name}" width="30px"></td>
+                    <td>${team.team.name}</td>
+                    <td>${team.playedGames}</td>
+                    <td>${team.won}</td>
+                    <td>${team.draw}</td>
+                    <td>${team.lost}</td>
+                    <td>${team.points}</td>
+                </tr>
+                `;
+                i++;
+
+            });
+            contents.innerHTML = `
+                <div class="card">
+                    <table class="stripped responsive-table">
+                        <thead>
+                            <th></th>
+                            <th></th>
+                            <th>Nama Tim</th>
+                            <th>PG</th>
+                            <th>W</th>
+                            <th>D</th>
+                            <th>L</th>
+                            <th>P</th>
+                        </thead>
+                        <tbody>
+                            ${teams}
+                        </tbody>
+                    </table>
+                </div>
+            `;
+        }).catch(err => {
+            console.error(err);
+        })
+}
+
+function getListMatches() {
+    title.innerHTML = "Jadwal Pertandingan Liga Primer Inggris";
+    fetch(matchEndPoin, fetchHeader)
+        .then(response => response.json())
+        .then(resJson => {
+            console.log(resJson.matches);
+            let matchs = "";
+            let i = 1;
+            resJson.matches.forEach(match => {
+                let d = new Date(match.utcDate).toLocaleDateString("id");
+                let scoreHomeTeam = (match.score.fullTime.homeTeam == null ? 0 : match.score.fullTime.homeTeam);
+                let scoreAwayTeam = (match.score.fullTime.awayTeam == null ? 0 : match.score.fullTime.awayTeam);
+                matchs += `
+                <tr>
+                    <td style="padding-left:20px;">${i}.</td>
+                    <td>${match.homeTeam.name} vs ${match.awayTeam.name}</td>
+                    <td>${d}</td>
+                    <td>${scoreHomeTeam}:${scoreAwayTeam}</td>
+                </tr>
+                `;
+                i++;
+
+            });
+            contents.innerHTML = `
+                <div class="card">
+                    <table class="stripped responsive-table">
+                        <thead>
+                            <th></th>
+                            <th>Peserta</th>
+                            <th>Tanggal</th>
+                            <th>Skor Akhir</th>
+                        </thead>
+                        <tbody>
+                            ${matchs}
+                        </tbody>
+                    </table>
+                </div>
+            `;
+        }).catch(err => {
+            console.error(err);
+        })
+}
+
+function loadPage(page) {
+    switch (page) {
+        case "teams":
+            getListTeams();
+            break;
+        case "standings":
+            getListStandings();
+            break;
+        case "matches":
+            getListMatches();
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
+
+
+    document.querySelectorAll(".sidenav a, .topnav a").forEach(elm => {
+        elm.addEventListener("click", evt => {
+            let sideNav = document.querySelector(".sidenav");
+            M.Sidenav.getInstance(sideNav).close();
+            page = evt.target.getAttribute("href").substr(1);
+            loadPage(page);
+        })
+    })
+    var page = window.location.hash.substr(1);
+    if (page === "" || page === "!") page = "teams";
+    loadPage(page);
+});
